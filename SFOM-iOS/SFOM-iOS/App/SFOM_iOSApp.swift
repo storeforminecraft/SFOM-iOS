74d0dacfcf372785973e34cd8c6fdb1e84b0893f
Jeonhui <dlwjsgml02@naver.com>
2022-11-18 13:11:20 +0900

    feat: update Assets & architecture

--- SFOM-iOS/SFOM-iOS/App/SFOM_iOSApp.swift
+++ SFOM-iOS/SFOM-iOS/App/SFOM_iOSApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct SFOM_iOSApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MainView()
         }
     }
 }
---
+++ SFOM-iOS/SFOM-iOS/Presentation/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/11/18.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationView {
+            TabView {
+                
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
