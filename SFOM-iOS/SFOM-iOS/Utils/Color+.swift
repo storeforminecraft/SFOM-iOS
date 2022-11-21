ca6de7fbc4474c7854f00b81b145d8e1f3ccf649
Jeonhui <dlwjsgml02@naver.com>
2022-11-21 14:01:59 +0900

    feat: add Extension & LaunchScreen

---
+++ SFOM-iOS/SFOM-iOS/Utils/Color+.swift
@@ -0,0 +1,17 @@
+//
+//  Color+.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/11/18.
+//
+
+import SwiftUI
+
+extension Color  {
+    static let accentColor: Color = Color("AccentColor")
+    static let accentDeepColor: Color = Color("AccentDeepColor")
+    static let borderColor: Color = Color("BorderColor")
+    static let textColor: Color = Color("PastelColor")
+}
+
+
---
+++ SFOM-iOS/SFOM-iOS/Utils/String+.swift
@@ -0,0 +1,20 @@
+//
+//  String+.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/11/18.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        NSLocalizedString(self, comment: "")
+    }
+    
+    public subscript(r: Int, l: Int = Int.max) -> String {
+        let startIndex = self.index(self.startIndex, offsetBy: r)
+        let endIndex = self.index(self.startIndex, offsetBy: l == Int.max ? r+1 : min(l, self.count))
+        return String(self[startIndex..<endIndex])
+    }
+}
