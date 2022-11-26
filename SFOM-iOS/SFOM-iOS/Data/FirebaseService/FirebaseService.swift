7c5a3c6354f69f78fd70ccea75ba2119fb800082
Jeonhui <dlwjsgml02@naver.com>
2022-11-26 21:11:51 +0900

    feat: setup FirebaseService

---
+++ SFOM-iOS/SFOM-iOS/Data/FirebaseService/FirebaseService.swift
@@ -0,0 +1,30 @@
+//
+//  FirebaseService.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/11/25.
+//
+
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+final class FirebaseService {
+    static let shared = FirebaseService()
+    let auth: Auth
+    let firestore: Firestore
+    var ref: DatabaseReference
+
+    private init() {
+        FirebaseApp.configure()
+        auth = Auth.auth()
+        firestore = Firestore.firestore()
+
+        let db = Database.database()
+        db.isPersistenceEnabled = true
+        ref = db.reference(fromURL: "https://storeforminecraft.firebaseio.com")
+        ref.keepSynced(true)
+    }
+}
