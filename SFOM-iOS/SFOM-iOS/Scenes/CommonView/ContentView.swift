35a97e8c6e12190b6025bc1d7df39cdf7f62fb9a
Jeonhui <dlwjsgml02@naver.com>
2022-12-02 01:36:40 +0900

    feat: create CommonViews (Content, Post)

---
+++ SFOM-iOS/SFOM-iOS/Scenes/CommonView/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/12/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SFOM-iOS/SFOM-iOS/Scenes/CommonView/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/12/02.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}
