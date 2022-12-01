d2f081ba9b50cfff43c31507513bbc176dfd4a2d
Jeonhui <dlwjsgml02@naver.com>
2022-12-01 20:02:06 +0900

    feat: create ProfileView

---
+++ SFOM-iOS/SFOM-iOS/Scenes/CommonView/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/12/01.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    let uid: String
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView(uid: "11111")
+    }
+}
