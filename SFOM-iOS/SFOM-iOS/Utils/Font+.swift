d35ca66d00e143e18610da8ebd5a8e422a0f4696
Jeonhui <dlwjsgml02@naver.com>
2022-12-01 19:48:11 +0900

    feat: update font extension

--- SFOM-iOS/SFOM-iOS/Utils/Font+.swift
+++ SFOM-iOS/SFOM-iOS/Utils/Font+.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 extension Font {
+    static let SFOMLargeTitleFont: Font = .system(size: 32, weight: .bold)
+    
     static let SFOMTitleFont: Font = .system(size: 28, weight: .bold)
+    static let SFOMMediumFont: Font = .system(size: 24)
     static let SFOMSmallFont: Font = .system(size: 18)
 }
