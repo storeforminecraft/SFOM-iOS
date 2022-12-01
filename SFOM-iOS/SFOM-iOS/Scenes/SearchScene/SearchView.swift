2317be5755b9a006fc82db194b904080aa7a736f
Jeonhui <dlwjsgml02@naver.com>
2022-12-02 01:39:16 +0900

    feat: update SearchView

--- SFOM-iOS/SFOM-iOS/Scenes/SearchScene/SearchView.swift
+++ SFOM-iOS/SFOM-iOS/Scenes/SearchScene/SearchView.swift
@@ -7,9 +7,43 @@
 
 import SwiftUI
 
+final class SearchViewModel: ObservableObject {
+    @Published var searchText: String = ""
+    @Published var searchData: [String] = []
+    @Published var isSearching: Bool = false {
+        didSet {
+            if !isSearching {
+                searchData = []
+            }
+        }
+    }
+
+    func search() {
+        
+    }
+
+}
+
 struct SearchView: View {
+    @ObservedObject private var searchViewModel = SearchViewModel()
+
     var body: some View {
-        Text("SearchView")
+        VStack (alignment: .leading) {
+            if !searchViewModel.isSearching {
+                Text(LocalizedString.SearchView.searchTitle)
+                    .font(.SFOMTitleFont)
+            }
+
+            SFOMSearchBar(placeholder: LocalizedString.SearchView.searchPlaceholder,
+                          text: $searchViewModel.searchText,
+                          state: $searchViewModel.isSearching) {
+
+            }
+
+            Spacer()
+            HStack { Spacer() }
+        }
+            .padding()
     }
 }
 
