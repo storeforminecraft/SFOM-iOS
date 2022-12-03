bb5e80c46a1bd3917b6b93a798b113c7f388bffd
Jeonhui <dlwjsgml02@naver.com>
2022-12-03 23:33:35 +0900

    feat: update SearchView & create SearchData struct

---
+++ SFOM-iOS/SFOM-iOS/Models/SearchData.swift
@@ -0,0 +1,79 @@
+//
+//  SearchContent.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/12/03.
+//
+
+import Foundation
+
+struct SearchData: Codable {
+    let authorUid: String
+    let category: String
+    let createdTimestamp: Date
+    let downloadCount: Int
+    let id: String
+    let likeCount: Int
+    let modifiedTimestamp: Date
+    let state: String
+    let tags: [String]
+    private let _desc: [String: String]
+    private let _name: [String: String]
+
+    var desc: String {
+        guard let tempValue = _desc.first?.value else { return "" }
+        return _desc[LocalizedString.location] ?? tempValue
+    }
+    
+    var name: String {
+        guard let tempValue = _name.first?.value else { return "" }
+        return _name[LocalizedString.location] ?? tempValue
+    }
+
+    enum CodingKeys: CodingKey {
+        case authorUid, category, createdTimestamp, downloadCount, id, likeCount, modifiedTimestamp, state, tags
+        case desc_en, desc_ko
+        case name_en, name_ko
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.authorUid = try container.decode(String.self, forKey: .authorUid)
+        self.category = try container.decode(String.self, forKey: .category)
+        self.createdTimestamp = try container.decode(Date.self, forKey: .createdTimestamp)
+        self.downloadCount = try container.decode(Int.self, forKey: .downloadCount)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.likeCount = try container.decode(Int.self, forKey: .likeCount)
+        self.modifiedTimestamp = try container.decode(Date.self, forKey: .modifiedTimestamp)
+        self.state = try container.decode(String.self, forKey: .state)
+        self.tags = try container.decode([String].self, forKey: .tags)
+
+        let desc_en = try container.decode(String.self, forKey: .desc_en)
+        let desc_ko = try container.decode(String.self, forKey: .desc_ko)
+        self._desc = ["en": desc_en, "ko": desc_ko]
+
+        let name_en = try container.decode(String.self, forKey: .name_en)
+        let name_ko = try container.decode(String.self, forKey: .name_ko)
+        self._name = ["en": name_en, "ko": name_ko]
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(authorUid, forKey: .authorUid)
+        try container.encode(category, forKey: .category)
+        try container.encode(createdTimestamp, forKey: .createdTimestamp)
+        try container.encode(downloadCount, forKey: .downloadCount)
+        try container.encode(id, forKey: .id)
+        try container.encode(likeCount, forKey: .likeCount)
+        try container.encode(modifiedTimestamp, forKey: .modifiedTimestamp)
+        try container.encode(state, forKey: .state)
+        try container.encode(tags, forKey: .tags)
+
+        try container.encode(_desc["en"], forKey: .desc_en)
+        try container.encode(_desc["ko"], forKey: .desc_en)
+        try container.encode(_name["en"], forKey: .name_en)
+        try container.encode(_name["ko"], forKey: .name_ko)
+    }
+}
--- SFOM-iOS/SFOM-iOS/Scenes/SearchScene/SearchView.swift
+++ SFOM-iOS/SFOM-iOS/Scenes/SearchScene/SearchView.swift
@@ -6,26 +6,65 @@
 //
 
 import SwiftUI
+import Combine
+import Alamofire
+import Kingfisher
 
 final class SearchViewModel: ObservableObject {
     @Published var searchText: String = ""
-    @Published var searchData: [String] = []
-    @Published var isSearching: Bool = false {
-        didSet {
-            if !isSearching {
-                searchData = []
-            }
-        }
+    @Published var searchData: [Resource] = []
+    @Published var isSearching: Bool = false
+    @Published var pagenation: Int = 1
+    @Published var tapReturn: Void = ()
+
+    var cancelBag = Set<AnyCancellable>()
+
+    init() {
+        self.bind()
+    }
+
+    func bind() {
+        $isSearching.sink { receive in
+            if receive { self.search() }
+            else { self.reset() }
+        }.store(in: &cancelBag)
+    }
+
+    func reset() {
+        self.searchData = []
+        self.pagenation = 1
     }
 
     func search() {
-        
+        AF.request("https://asia-northeast3-storeforminecraft.cloudfunctions.net/v1/resources",
+                   method: .get,
+                   parameters: ["keyword": searchText,
+                                "pagenation": pagenation],
+                   encoding: URLEncoding.queryString)
+            .responseDecodable(of: [SearchData].self) { response in
+            if let reponseData = response.value {
+                reponseData
+                    .publisher
+                    .flatMap { searchData in
+                    FirebaseManager.shared.firestore.collection("resources")
+                        .document(searchData.id)
+                        .publisher(type: Resource.self)
+                }.compactMap { $0 }
+                    .sink { data in
+                    self.searchData.append(data)
+                }.store(in: &self.cancelBag)
+            }
+        }
     }
-
 }
 
 struct SearchView: View {
     @ObservedObject private var searchViewModel = SearchViewModel()
+
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
 
     var body: some View {
         VStack (alignment: .leading) {
@@ -36,14 +75,33 @@ struct SearchView: View {
 
             SFOMSearchBar(placeholder: LocalizedString.SearchView.searchPlaceholder,
                           text: $searchViewModel.searchText,
-                          state: $searchViewModel.isSearching) {
-
-            }
-
-            Spacer()
+                          state: $searchViewModel.isSearching)
+            searchContents
             HStack { Spacer() }
         }
             .padding()
+            .ignoresSafeArea(edges: .bottom)
+    }
+
+    var searchContents: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 10) {
+                ForEach(searchViewModel.searchData,
+                        id: \.id) { data in
+                    VStack {
+                        HStack { Spacer() }
+                        VStack {
+                            if let thumbImage = data.images.first {
+                                KFImage(URL(string:))
+                            }
+                        }
+
+                    }.background(.blue)
+                }
+            }
+                .background(.orange)
+                .padding(.bottom, 10)
+        }
     }
 }
 
