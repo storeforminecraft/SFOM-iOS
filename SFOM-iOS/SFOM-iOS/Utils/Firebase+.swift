712358ad1dcb3f5fa0d6be2a8c6ece74f7d9d7c1
Jeonhui <dlwjsgml02@naver.com>
2022-12-10 04:14:08 +0900

    feat: Firebase DocumentReference publisher

--- SFOM-iOS/SFOM-iOS/Utils/Firebase+.swift
+++ SFOM-iOS/SFOM-iOS/Utils/Firebase+.swift
@@ -9,24 +9,4 @@ import Foundation
 import Firebase
 import Combine
 
-extension DocumentReference {
-    func publisher<T: Decodable>(type: T.Type) -> AnyPublisher<T?, Never> {
-        Future<T?, Never> { promise in
-            self.getDocument(as: T.self) { data in
-                promise(.success(try? data.get()))
-            }
-        }.eraseToAnyPublisher()
-    }
 
-    var publisher: AnyPublisher<[String: Any]?, Never> {
-        return Future<[String: Any]?, Never> { promise in
-            self.getDocument { snapshotData, error in
-                if let data = snapshotData?.data() {
-                    promise(.success(data))
-                } else {
-                    promise(.success(nil))
-                }
-            }
-        }.eraseToAnyPublisher()
-    }
-}
