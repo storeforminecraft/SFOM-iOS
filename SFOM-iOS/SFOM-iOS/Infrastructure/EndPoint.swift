71a9851fb4806ad168ed1cad8108613e960dcc07
Jeonhui <dlwjsgml02@naver.com>
2022-12-10 04:12:39 +0900

    feat: EndPoint Protocol & SFOMEndPoint

---
+++ SFOM-iOS/SFOM-iOS/Infrastructure/EndPoint.swift
@@ -0,0 +1,26 @@
+//
+//  EndPoint.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/12/10.
+//
+
+import Foundation
+
+protocol MainCollection {
+    var path: String { get }
+}
+
+protocol SubCollection {
+    var parent: MainCollection { get }
+    var path: String { get }
+}
+
+protocol Document {
+    var isCurrentUser: Bool { get }
+    var path: String { get }
+}
+
+protocol EndPoint {
+    var ref: (MainCollection, Document?, SubCollection?, Document?) { get }
+}
---
+++ SFOM-iOS/SFOM-iOS/Infrastructure/SFOMEndPoint.swift
@@ -0,0 +1,94 @@
+//
+//  SFOMEndPoint.swift
+//  SFOM-iOS
+//
+//  Created by 이전희 on 2022/12/10.
+//
+
+import Foundation
+
+public struct SFOMEndPoint: EndPoint {
+    var collection: MainCollection
+    var document: Document?
+    var subCollection: SubCollection?
+    var subDocument: Document?
+
+    init?(collection: MainCollection,
+          document: Document?,
+          subCollection: (any SubCollection)? = nil,
+          subDocument: Document? = nil) {
+        guard let collection = collection as? SFOMEndPoint.SFOMCollection else { return nil }
+        if let subCollectionParent = subCollection?.parent {
+            guard document != nil else { return nil }
+            guard let subCollectionParent = subCollectionParent as? SFOMEndPoint.SFOMCollection,
+                subCollectionParent != collection else { return nil }
+        }
+
+        self.collection = collection
+        self.document = document
+        self.subCollection = subCollection
+        self.subDocument = subDocument
+    }
+
+    var ref: (MainCollection, Document?, SubCollection?, Document?) {
+        return (collection, document, subCollection, subDocument)
+    }
+}
+
+extension SFOMEndPoint {
+    enum SFOMCollection: String, MainCollection {
+        case posts
+        case ranks_daily
+        case ranks_monthly
+        case reports
+        case resources
+        case playlists
+        case favorites
+        case favorites_resource
+
+        var path: String {
+            return self.rawValue
+        }
+    }
+
+    enum Favorites: String, SubCollection {
+        case favorites_playlist
+        var parent: MainCollection { SFOMEndPoint.SFOMCollection.favorites }
+        var path: String { self.rawValue }
+    }
+
+    enum Playlists: String, SubCollection {
+        case resources_playlist
+        var parent: MainCollection { SFOMEndPoint.SFOMCollection.playlists }
+        var path: String { self.rawValue }
+    }
+
+    enum Ranks_daily: String, SubCollection {
+        case rankdatas
+        var parent: MainCollection { SFOMEndPoint.SFOMCollection.ranks_daily }
+        var path: String { self.rawValue }
+    }
+
+    enum Ranks_mothly: String, SubCollection {
+        case rankdatas
+        var parent: MainCollection { SFOMEndPoint.SFOMCollection.ranks_monthly }
+        var path: String { self.rawValue }
+    }
+
+    enum SFOMDocument: Document {
+        case currentUser
+        case another(_ documentPath: String)
+
+        var isCurrentUser: Bool {
+            if case .currentUser = self { return true }
+            return false
+        }
+
+        var path: String {
+            switch self {
+            case .currentUser: return ""
+            case let .another(path): return path
+            }
+        }
+    }
+}
