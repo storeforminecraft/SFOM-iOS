080c917c68450f9cf32feb192498b55fb3d4a664
Jeonhui <dlwjsgml02@naver.com>
2022-12-11 05:17:36 +0900

    feat: signUpPublisher

--- SFOM-iOS/SFOM-iOS/Infrastructure/FirebaseService/Firebase+/Auth+Publisher.swift
+++ SFOM-iOS/SFOM-iOS/Infrastructure/FirebaseService/Firebase+/Auth+Publisher.swift
@@ -9,6 +9,7 @@ import FirebaseAuth
 import Combine
 
 extension Auth {
+    // MARK: - Sign In
     func signInPublisher(email: String, password: String) -> AnyPublisher<AuthDataResult, Error> {
         return Future<AuthDataResult, Error>{ [weak self] promise in
             guard let self = self else {
@@ -27,6 +28,7 @@ extension Auth {
         }.eraseToAnyPublisher()
     }
     
+    // MARK: - Sign Out
     func signOutPublisher() -> AnyPublisher<Bool, Error> {
         return Future<Bool, Error> { [weak self] promise in
             guard let self = self else {
@@ -42,6 +44,26 @@ extension Auth {
         }.eraseToAnyPublisher()
     }
     
+    // MARK: - Sign Up
+    func signUpPublisher(email: String, password: String) -> AnyPublisher<AuthDataResult, Error> {
+        return Future<AuthDataResult, Error> { [weak self] promise in
+            guard let self = self else {
+                promise(.failure(FirebaseCombineError.objectError))
+                return
+            }
+            self.createUser(withEmail: email, password: password) { authDataResult, error in
+                if let error = error {
+                    promise(.failure(error))
+                } else if let authDataResult = authDataResult {
+                    promise(.success(authDataResult))
+                } else {
+                    promise(.failure(FirebaseCombineError.noDataError))
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    // MARK: - Withdrawal
     func withdrawalPublisher() -> AnyPublisher<Bool,Error> {
         return Future<Bool, Error> { [weak self] promise in
             guard let self = self else {
