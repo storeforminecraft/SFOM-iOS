53199f63d0175dfde78a2807832255eb357260ab
Jeonhui <dlwjsgml02@naver.com>
2022-12-11 03:18:16 +0900

    [fix] update Service

--- SFOM-iOS/SFOM-iOS/Infrastructure/FirebaseService/FirebaseService.swift
+++ SFOM-iOS/SFOM-iOS/Infrastructure/FirebaseService/FirebaseService.swift
@@ -14,52 +14,92 @@ import FirebaseFirestoreSwift
 
 final class FirebaseService {
     static let shared = FirebaseService()
+    let uid = CurrentValueSubject<String?, Never>(nil)
     let auth: Auth
     let firestore: Firestore
     var reference: DatabaseReference
-
+    
     private init() {
         FirebaseApp.configure()
         auth = Auth.auth()
         firestore = Firestore.firestore()
-
+        
         let database = Database.database()
         database.isPersistenceEnabled = true
         reference = database.reference(fromURL: "https://storeforminecraft.firebaseio.com")
         reference.keepSynced(true)
+        
+        uid.send(auth.currentUser?.uid)
     }
 }
 
 private extension FirebaseService {
-    func getRefence(endPoint: EndPoint){}
+    func documentReference<E: EndPoint>(endPoint: E) -> DocumentReference? {
+        let collectionPath = endPoint.reference.collection.path
+        guard let documet = endPoint.reference.document,
+              let documnetPath = (documet.isCurrentUser ? uid.value : documet.path) else { return nil }
+        
+        guard let subCollectionPath = endPoint.reference.subCollection?.path else {
+            return firestore
+                .collection(collectionPath)
+                .document(documnetPath)
+        }
+        guard let subDocumet = endPoint.reference.subDocument,
+              let subDocumnetPath = (documet.isCurrentUser ? uid.value : subDocumet.path) else { return nil }
+        
+        return firestore
+            .collection(collectionPath)
+            .document(documnetPath)
+            .collection(subCollectionPath)
+            .document(subDocumnetPath)
+    }
+    
+    func collectionReference<E: EndPoint>(endPoint: E) -> CollectionReference? {
+        let collectionPath = endPoint.reference.collection.path
+        guard let documet = endPoint.reference.document,
+              let documnetPath = (documet.isCurrentUser ? uid.value : documet.path) else { return nil }
+        
+        guard let subCollectionPath = endPoint.reference.subCollection?.path else {
+            return firestore
+                .collection(collectionPath)
+        }
+        
+        return firestore
+            .collection(collectionPath)
+            .document(documnetPath)
+            .collection(subCollectionPath)
+    }
 }
 
 extension FirebaseService: NetworkService {
-    func signIn(email: String, password: String) {
+    func signIn(email: String, password: String){
+        // self.saltPassword(email: email, password: password)
 
+        
+        // auth.signIn(withEmail: email, password: password, completion: <#T##((AuthDataResult?, Error?) -> Void)?##((AuthDataResult?, Error?) -> Void)?##(AuthDataResult?, Error?) -> Void#>)
     }
-
+    
     func signOut() {
-
+        auth.signOut()
     }
     
     func withdrawal() {
         
     }
     
-    func create<T>(endPoint: EndPoint, dto: T) where T : DTO {
+    func create<T>(endPoint: EndPoint, dto: T) where T: DTO {
         
     }
     
-    func read<T>(endPoint: EndPoint, dto: T) where T : DTO {
+    func read<T>(endPoint: EndPoint, dto: T) where T: DTO {
         
     }
     
-    func update<T>(endPoint: EndPoint, dto: T) where T : DTO {
+    func update<T>(endPoint: EndPoint, dto: T) where T: DTO {
         
     }
     
-    func delete<T>(endPoint: EndPoint, dto: T) where T : DTO {
+    func delete<T>(endPoint: EndPoint, dto: T) where T: DTO {
         
     }
 }
--- SFOM-iOS/SFOM-iOS/Infrastructure/NetworkService.swift
+++ SFOM-iOS/SFOM-iOS/Infrastructure/NetworkService.swift
@@ -5,9 +5,10 @@
 //  Created by 이전희 on 2022/12/10.
 //
 
-import Foundation
+import Combine
 
 protocol NetworkService {
+    var uid: CurrentValueSubject<String?, Never> { get }
     func signIn(email: String, password: String)
     func signOut()
     func withdrawal()
