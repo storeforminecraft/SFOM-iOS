9fa7082c18b15d089f6b129090dbaa1c6b961a3b
Jeonhui <dlwjsgml02@naver.com>
2022-12-11 04:03:21 +0900

    feat: move Firebase File

--- SFOM-iOS/SFOM-iOS/Infrastructure/FirebaseService/FirebaseService.swift
+++ SFOM-iOS/SFOM-iOS/Infrastructure/FirebaseService/FirebaseService.swift
@@ -6,6 +6,7 @@
 //
 
 import Combine
+import CryptoKit
 import Firebase
 import FirebaseAuth
 import FirebaseStorage
@@ -72,18 +73,26 @@ private extension FirebaseService {
 }
 
 extension FirebaseService: NetworkService {
-    func signIn(email: String, password: String){
-        // self.saltPassword(email: email, password: password)
-
+    func signIn(email: String, password: String) -> AnyPublisher<Bool, Error> {
+        return self.saltPassword(email: email, password: password)
+            .flatMap { password in
+                guard let password = password else {
+                    return Just<Bool>(false)
+                    .setFailureType(to: Error.self)
+                    .eraseToAnyPublisher()
+                }
+                return self.auth.signInPublisher(email: email, password: password)
+                    .map { _ in true }
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func signOut() -> AnyPublisher<Bool, Error> {
         
-        // auth.signIn(withEmail: email, password: password, completion: <#T##((AuthDataResult?, Error?) -> Void)?##((AuthDataResult?, Error?) -> Void)?##(AuthDataResult?, Error?) -> Void#>)
     }
     
-    func signOut() {
-        auth.signOut()
-    }
-    
-    func withdrawal() {
+    func withdrawal() -> AnyPublisher<Bool, Error> {
         
     }
     
@@ -101,5 +110,48 @@ extension FirebaseService: NetworkService {
     
     func delete<T>(endPoint: EndPoint, dto: T) where T: DTO {
         
+    }
+}
+
+private extension FirebaseService {
+    func saltPassword(email: String, password: String) -> AnyPublisher<String?, Never> {
+        guard let emailData = email.data(using: .utf8) else {
+            return Just<String?>(nil)
+                .eraseToAnyPublisher()
+        }
+        let child = SHA256.hash(data: emailData).compactMap { String(format: "%02x", $0) }.joined()
+        
+        return self.reference.child("salts")
+            .child(child)
+            .getDataPublisher
+            .timeout(.seconds(5), scheduler: DispatchQueue.global())
+            .catch { _ in Just<DataSnapshot?>(nil).eraseToAnyPublisher() }
+            .map { [weak self] dataSnapshot -> String? in
+                guard let self = self else { return nil }
+                guard let dataSnapshotValue = dataSnapshot?.value else { return nil }
+                
+                var salt = ""
+                if dataSnapshot!.value! is NSNull {
+                    guard let sha1Email = self.sha1Hash(data: emailData).lastWord else { return nil }
+                    salt = String(sha1Email.prefix(5))
+                } else {
+                    guard let salt = (dataSnapshotValue as? NSDictionary)?["salt"] as? String else { return nil }
+                }
+                
+                guard let firstHashingData = "MCPE STORE\(password)ver2".data(using: .utf8) else { return nil }
+                guard let firstHashing = self.sha1Hash(data: firstHashingData).lastWord else { return nil }
+                guard let secondHashingData = "\(salt)\(firstHashing)MCPE_STORE_ver2".data(using: .utf8) else { return nil }
+                guard let secondHashing = self.sha2Hash(data: secondHashingData).lastWord else { return }
+                return secondHashing
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    private func sha1Hash(data: Data) -> String {
+        return Insecure.SHA1.hash(data: data).description
+    }
+    
+    private func sha2Hash(data: Data) -> String {
+        SHA512.hash(data: data).description
     }
 }
